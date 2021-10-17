bea4157ef81378dacdb6527b7eaaf89ad810fe96
Vicente Aguilera Pérez <vicente_prez@hotmail.com>
2021-10-17 01:05:10 -0500

    cifrado cesar

---
+++ src/ejemplo/chat_encriptado/cifradocesar/CifradoCesar.java
@@ -0,0 +1,56 @@
+package ejemplo.chat_encriptado.cifradocesar;
+
+public class CifradoCesar
+{
+    //método para cifrar el texto
+    public static String cifradoCesar(String texto, int codigo) {
+        StringBuilder cifrado = new StringBuilder();
+        codigo = codigo % 26;
+        for (int i = 0; i < texto.length(); i++) {
+            if (texto.charAt(i) >= 'a' && texto.charAt(i) <= 'z') {
+                if ((texto.charAt(i) + codigo) > 'z') {
+                    cifrado.append((char) (texto.charAt(i) + codigo - 26));
+                } else {
+                    cifrado.append((char) (texto.charAt(i) + codigo));
+                }
+            } else if (texto.charAt(i) >= 'A' && texto.charAt(i) <= 'Z') {
+                if ((texto.charAt(i) + codigo) > 'Z') {
+                    cifrado.append((char) (texto.charAt(i) + codigo - 26));
+                } else {
+                    cifrado.append((char) (texto.charAt(i) + codigo));
+                }
+            }
+            else
+            {
+                cifrado.append(texto.charAt(i));
+            }
+        }
+        return cifrado.toString();
+    }
+
+    //método para descifrar el texto
+    public static String descifradoCesar(String texto, int codigo) {
+        StringBuilder cifrado = new StringBuilder();
+        codigo = codigo % 26;
+        for (int i = 0; i < texto.length(); i++) {
+            if (texto.charAt(i) >= 'a' && texto.charAt(i) <= 'z') {
+                if ((texto.charAt(i) - codigo) < 'a') {
+                    cifrado.append((char) (texto.charAt(i) - codigo + 26));
+                } else {
+                    cifrado.append((char) (texto.charAt(i) - codigo));
+                }
+            } else if (texto.charAt(i) >= 'A' && texto.charAt(i) <= 'Z') {
+                if ((texto.charAt(i) - codigo) < 'A') {
+                    cifrado.append((char) (texto.charAt(i) - codigo + 26));
+                } else {
+                    cifrado.append((char) (texto.charAt(i) - codigo));
+                }
+            }
+            else
+            {
+                cifrado.append(texto.charAt(i));
+            }
+        }
+        return cifrado.toString();
+    }
+}
